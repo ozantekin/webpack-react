33c8dd08487b568b5981ee300c17f64d600dd9ad
Ozan Tekin <ardilozantekin@gmail.com>
2022-12-09 18:28:03 +0300

    add webpack tutorial

---
+++ build/main.js
@@ -0,0 +1,1 @@
+(()=>{const e=document.createElement("div");e.innerHTML="Merhaba Dünya!",document.body.append(e)})();
---
+++ src/Selam.jsx
@@ -0,0 +1,5 @@
+import './Selam.css'
+
+const Selam = () => <h1 className='selam'>Selam!</h1>
+
+export default Selam
